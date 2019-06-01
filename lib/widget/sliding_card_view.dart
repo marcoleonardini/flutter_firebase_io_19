0a71a025231cab096f3e60a0939549b32bba5ebf
Marco Leonardini <marco_leonardini@hotmail.com>
2019-06-01 00:14:20 -0400

    Added firebase functions

--- lib/widget/sliding_card_view.dart
+++ lib/widget/sliding_card_view.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_io_19/widget/sliding_card.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SlidingCardsView extends StatefulWidget {
   SlidingCardsView({Key key}) : super(key: key);
@@ -9,15 +13,21 @@ class SlidingCardsView extends StatefulWidget {
 
 class _SlidingCardsViewState extends State<SlidingCardsView> {
   PageController pageController;
-  double pageOffset =0 ;
+  double pageOffset = 0;
+
+  File _imageFile;
+  List<Face> _faces = [];
+  List<Barcode> _barcodes = [];
+  VisionText _texts;
+  List<ImageLabel> _imageLabels = [];
 
   @override
   void initState() {
     super.initState();
     pageController = PageController(viewportFraction: 0.8);
-    pageController.addListener((){
+    pageController.addListener(() {
       setState(() {
-        pageOffset = pageController.page; 
+        pageOffset = pageController.page;
       });
     });
   }
@@ -40,31 +50,87 @@ class _SlidingCardsViewState extends State<SlidingCardsView> {
             description: '4-20-30',
             assetName: 'logo-logomark.png',
             offset: pageOffset,
-            onPressed: (){},
+            onPressed: () {},
           ),
           SlidingCard(
             title: 'Ml-Kit',
             description: '4-20-30',
             assetName: 'detection.jpg',
-            offset: pageOffset-1,
-            onPressed: (){},
+            offset: pageOffset - 1,
+            onPressed: () {},
           ),
           SlidingCard(
             title: 'Ml-Kit',
             description: '4-20-30',
             assetName: 'detection.jpg',
-            offset: pageOffset-2,
-            onPressed: (){},
+            offset: pageOffset - 2,
+            onPressed: () {},
           ),
           SlidingCard(
             title: 'Ml-Kit',
             description: '4-20-30',
             assetName: 'detection.jpg',
-            offset: pageOffset-3,
-            onPressed: (){},
+            offset: pageOffset - 3,
+            onPressed: () {},
           )
         ],
       ),
     );
+  }
+
+  void _getImageAndDetectBarcodes() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final barCodeDetector = FirebaseVision.instance.barcodeDetector();
+    final barcodes = await barCodeDetector.detectInImage(image);
+    if (mounted) {
+      setState(() {
+        print('mounted');
+        _imageFile = imageFile;
+        _barcodes = barcodes;
+      });
+    }
+  }
+
+  void _getImageAndDetectText() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final barCodeDetector = FirebaseVision.instance.textRecognizer();
+    final texts = await barCodeDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        print('mounted');
+        _imageFile = imageFile;
+        _texts = texts;
+      });
+    }
+  }
+
+  void _getImageAndDetectFaces() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final barCodeDetector = FirebaseVision.instance.faceDetector();
+    final faces = await barCodeDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        print('mounted');
+        _imageFile = imageFile;
+        _faces = faces;
+      });
+    }
+  }
+
+  void _getImageAndDetectImageLabeler() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final barCodeDetector = FirebaseVision.instance.imageLabeler();
+    final imageLabels = await barCodeDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        print('mounted');
+        _imageFile = imageFile;
+        _imageLabels = imageLabels;
+      });
+    }
   }
 }
